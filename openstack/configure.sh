21f7b688f94567824e12243408b01e979e7a0a58
Dan Higham <dan.higham@gmail.com>
2022-03-08 13:27:48 -0800

    Remove dhclient

--- openstack/configure.sh
+++ openstack/configure.sh
@@ -22,102 +22,7 @@ cat > /etc/network/interfaces <<-EOF
 EOF
 
 step 'Configure dhclient'
-cat > /etc/dhcp/dhclient.conf <<-EOF
-option rfc3442-classless-static-routes code 121 = array of unsigned integer 8;
-
-send host-name = gethostname();
-
-# prepend domain-name-servers 127.0.0.1;
-request subnet-mask, broadcast-address, time-offset, routers,
-    domain-name, domain-name-servers, domain-search, host-name,
-    dhcp6.name-servers, dhcp6.domain-search, dhcp6.fqdn, dhcp6.sntp-servers,
-    netbios-name-servers, netbios-scope, interface-mtu,
-    rfc3442-classless-static-routes, ntp-servers;
-# require subnet-mask, domain-name-servers;
-timeout 300;
-EOF
-
-cat > /etc/dhcp/dhclient-exit-hooks.d/rfc3442-classless-routes <<-EOF
-#!/bin/sh
-# set classless routes based on the format specified in RFC3442
-# e.g.:
-#   new_rfc3442_classless_static_routes='24 192 168 10 192 168 1 1 8 10 10 17 66 41'
-# specifies the routes:
-#   192.168.10.0/24 via 192.168.1.1
-#   10.0.0.0/8 via 10.10.17.66.41
-
-RUN="yes"
-
-if [ "$RUN" = "yes" ]; then
-    if [ -n "$new_rfc3442_classless_static_routes" ]; then
-        if [ "$reason" = "BOUND" ] || [ "$reason" = "REBOOT" ]; then
-
-            set -- $new_rfc3442_classless_static_routes
-
-            while [ $# -gt 0 ]; do
-                net_length=$1
-                via_arg=''
-
-                case $net_length in
-                    32|31|30|29|28|27|26|25)
-                        if [ $# -lt 9 ]; then
-                            return 1
-                        fi
-                        net_address="${2}.${3}.${4}.${5}"
-                        gateway="${6}.${7}.${8}.${9}"
-                        shift 9
-                        ;;
-                    24|23|22|21|20|19|18|17)
-                        if [ $# -lt 8 ]; then
-                            return 1
-                        fi
-                        net_address="${2}.${3}.${4}.0"
-                        gateway="${5}.${6}.${7}.${8}"
-                        shift 8
-                        ;;
-                    16|15|14|13|12|11|10|9)
-                        if [ $# -lt 7 ]; then
-                            return 1
-                        fi
-                        net_address="${2}.${3}.0.0"
-                        gateway="${4}.${5}.${6}.${7}"
-                        shift 7
-                        ;;
-                    8|7|6|5|4|3|2|1)
-                        if [ $# -lt 6 ]; then
-                            return 1
-                        fi
-                        net_address="${2}.0.0.0"
-                        gateway="${3}.${4}.${5}.${6}"
-                        shift 6
-                        ;;
-                    0)	# default route
-                        if [ $# -lt 5 ]; then
-                            return 1
-                        fi
-                        net_address="0.0.0.0"
-                        gateway="${2}.${3}.${4}.${5}"
-                        shift 5
-                        ;;
-                    *)	# error
-                        return 1
-                        ;;
-                esac
-
-                # take care of link-local routes
-                if [ "${gateway}" != '0.0.0.0' ]; then
-                    via_arg="via ${gateway}"
-                fi
-
-                # set route (ip detects host routes automatically)
-                ip -4 route add "${net_address}/${net_length}" \
-                    ${via_arg} dev "${interface}" >/dev/null 2>&1
-            done
-        fi
-    fi
-fi
-EOF
-chmod +x /etc/dhcp/dhclient-exit-hooks.d/rfc3442-classless-routes
+sed -e 
 
 # FIXME: remove root and alpine password
 step 'Set cloud configuration'
@@ -171,6 +76,7 @@ rc-update add acpid default
 rc-update add chronyd default
 rc-update add crond default
 rc-update add networking boot
+rc-update add staticroute boot
 rc-update add termencoding boot
 rc-update add sshd default
 rc-update add cloud-init default
